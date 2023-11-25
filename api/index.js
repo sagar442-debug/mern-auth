b0674689e48692ab8cb484f2a80c1bb677977af6
sagar442-debug <bnradlyp1@gmail.com>
2023-11-24 22:40:23 -0700

    Fixing deployment bug

--- api/index.js
+++ api/index.js
@@ -21,11 +21,11 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use(cors())
 
